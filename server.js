f980d12f7f57327e78f0e2c9d5faeed626cf472d
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2021-08-11 20:06:59 +0530

    Search feature added

--- server.js
+++ server.js
@@ -7,36 +7,21 @@ const { title } = require('process')
 const token = '1900703486:AAElE2nXiShEtnkkqi-43GJ4fzcPEMw-HS8'
 const bot = new TelegramBot(token, {polling: true})
 bot.setMyCommands([{command: "search", description:"Search for music"}])
-async function sendmusic(name, chatid, messageid) {
+
+async function sendmusicbyname(name, chatid, messageid) {
   const browser = await puppeteer.launch({ headless: true});
   try{
     console.log("Browser launched")
     const page = await browser.newPage()
-    // bot.sendMessage(chatid, "◂◌◌◌◌◌▸")
-    // await page.setViewport({
-    //   width:1366,
-    //   height:800,
-    //   deviceScaleFactor: 1
-    // })
-    // console.log("Page opened")
-    // await page.goto('https://youtube.com/results?search_query='+name)
-    // console.log("On YouTube")
-    // console.log("Searched for "+ name)
-    // bot.sendMessage(chatid, "◂●◌◌◌◌▸")
-    // await page.waitForTimeout(1000) //add network state
-    // const song = await page.evaluate(function(){
-    //   const youtubelink = document.querySelector('h3 > #video-title').getAttribute('href')
-    //   const title = document.querySelector('h3>#video-title yt-formatted-string').innerHTML.split("(")[0]
-    //   return {youtubelink, title}
-    // })
+    bot.sendMessage(chatid, "◂◌◌◌▸")
     const [song, response] = await Promise.all([
       getsong(name),
       page.goto('https://ytmp3.cc/downloader/')
     ])
-    console.log(song)
     console.log("Copied link for "+ song.title)
-    bot.sendMessage(chatid, "◂●◌◌▸")
     console.log("On ytmp3")
+    console.log(song)
+    bot.sendMessage(chatid, "◂●◌◌▸")
     await page.type('#input', `https://youtube.com${song.youtubelink}`)
     await page.click('#submit')
     console.log("Submitted youtube link")
@@ -51,52 +36,70 @@ async function sendmusic(name, chatid, messageid) {
     bot.sendAudio(chatid, song.downloadlink, {reply_to_message_id: messageid}).then(()=>{
       bot.sendMessage(chatid, "Here's "+ song.title)
     })
-    // fetch(downloadlink).then(res=>{
-    //   console.log("Response recieved")
-    //   bot.sendMessage(chatid, "◂●●●●●▸")
-    //   bot.sendMessage(chatid, "Downloading the music into myself")
-    //   return res.buffer()
-    // }).then(res=>{
-    //   console.log("Sending")
-    //   bot.sendMessage(chatid, "Sent it to you!")
-    //   bot.sendAudio(chatid, res, {reply_to_message_id: messageid}).then(()=>{
-    //     bot.sendMessage(chatid, "Here's "+ song.title)
-    //   })
-    // })
     await browser.close();
   }
   catch(err){
     console.log("Got an error" + err)
     bot.sendMessage(chatid, "I seem to have failed at that, please try again")
-    await browser.close();
+    await browser.close();  
   }
 }
 
-async function sendsearchresults(name, chatid, messageid){
-  const browser = await puppeteer.launch({ headless: false });
-  console.log("Browser launched")
-  const page = await browser.newPage()
-  await page.goto('https://youtube.com')
-  await page.type('#search', name)
-  const [response1] = await Promise.all([
-    page.waitForNavigation(),
-    page.click('#search-icon-legacy')
-  ])
-  await page.waitForTimeout(3000)
-  const songs = await page.evaluate(function(){
-    const youtubelinkelements = document.querySelectorAll('h3 > #video-title')
-    const youtubelinks = []
-    const title = []
-    for(let i = 0; i < 5; i++){
-      youtubelinks.push(youtubelinkelements[i].getAttribute('href'))
-      
-    }
-    // const title = document.querySelector('h3>#video-title yt-formatted-string').innerHTML.split("(")[0]
-    return {youtubelinks, title}
-  })
-  console.log(songs)
-  browser.close()
+
+async function sendmusicbyurl(youtubelink, chatid){
+  const browser = await puppeteer.launch({ headless: false});
+  try{
+    console.log("Browser launched")
+    const page = await browser.newPage()
+    await page.goto('https://ytmp3.cc/downloader/')
+    await page.type('#input', youtubelink)
+    await page.click('#submit')
+    await page.waitForTimeout(2000)
+    const downloadlink = await page.evaluate(function(){
+      return document.querySelector('#buttons > a').getAttribute('href')
+    })
+    const title = await page.evaluate(function(){
+      return document.querySelector('#title').innerHTML
+    })
+    console.log(downloadlink)
+    bot.sendAudio(chatid, downloadlink).then(()=>{
+      bot.sendMessage(chatid, "Here's "+title)
+    })
+    await browser.close()
+  }
+  catch(err){
+    console.log("Got an error" + err)
+    bot.sendMessage(chatid, "I seem to have failed at that, please try again")
+    await browser.close();  
+  }
 }
+async function sendsearchresults(name, chatid, messageid, iteration, num = 9){
+  var songs = await getsongs(name, num, iteration)
+  var reply = ""
+  var buttons = [[]]
+  let i = 0
+  for(i = 0; i<songs.length; i++){
+    reply+= `|${i+1 + (iteration-1)*num}|` + songs[i].title+"\n"
+    buttons[Math.floor(i/5)].push({text:String(i+1 + (iteration-1)*num), callback_data:songs[i].youtubelink})
+    if((i+1)%5 == 0){
+      buttons.push([])
+    }
+  }
+  buttons[Math.floor(i/5)].push({text:"More", callback_data:`/${name}/`+String(iteration+1)})
+  bot.sendMessage(chatid, reply, {reply_to_message_id: messageid, reply_markup: { inline_keyboard: buttons }})
+}
+
+bot.on('callback_query', async query=>{
+  // console.log(query)
+  console.log(query.data)
+  if(query.data[0] == '/'){
+    await sendsearchresults(query.data.split(`/`)[1],query.from.id,undefined,Number(query.data.split(`/`)[2]))
+    bot.answerCallbackQuery(query.id)
+  }else{
+    await sendmusicbyurl(query.data, query.from.id)
+    bot.answerCallbackQuery(query.id)
+  }
+})
 
 bot.on('message', async message=>{
   console.log(message)
@@ -106,16 +109,42 @@ bot.on('message', async message=>{
     }
     else if(message.text.startsWith("/search")){
       console.log(message.text)
-      sendsearchresults(message.text.slice(8))
+      sendsearchresults(message.text.slice(8), message.from.id, message.message_id, 1)
     }
   }
   else{
     bot.sendMessage(message.from.id, "Please wait")
-    sendmusic(message.text, message.from.id, message.message_id)
+    sendmusicbyname(message.text, message.from.id, message.message_id)
   }
   console.log( `${message.from.first_name} ${message.from.last_name} says ${message.text}`)
 })
 
+
+async function getsongs(input, num = 10, iteration = 1){
+  var searchterm = ""
+  input=input.split(" ")
+  for(let i = 0; i<input.length-1; i++){
+      searchterm+= (input[i]+'+')
+  }
+  searchterm+=input[input.length-1]
+  console.log("Getting results for")
+  console.log(searchterm)
+  console.log(num*iteration)
+  const res = await request('https://youtube.com/results?search_query='+searchterm)
+  var songs = []
+  var html = String(res)
+  var lastindex = 0
+  for (let i = 0; i<num*iteration; i++){
+    const indexoftitle = html.indexOf('"title":{"runs":[{"text":', lastindex)+26
+    lastindex = indexoftitle
+    const indexoflink = html.indexOf("/watch?", indexoftitle)
+    var youtubelink = "https://youtube.com" + html.slice(indexoflink, indexoflink+20)
+    var title = html.slice(indexoftitle, indexoftitle+100).split('"}')[0]
+    songs.push({title:title, youtubelink:youtubelink})
+  }
+  console.log(songs)
+  return songs.slice(-num)
+}
 
 async function getsong(input){
   var searchterm = ""
@@ -134,6 +163,6 @@ async function getsong(input){
   const indexoftitle = html.indexOf('"title":{"runs":[{"text":')+26
   song.youtubelink = "https://youtube.com" + html.slice(indexoflink, indexoflink+20)
   song.title = html.slice(indexoftitle, indexoftitle+100).split('"}')[0]
-  console.log(song)
+  // console.log(song)
   return song
 }
