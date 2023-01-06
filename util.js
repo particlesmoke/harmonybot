6a9fcccc5e3982dca4eee8082c11702a28621853
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2023-01-07 03:39:26 +0530

    recover after browser crash

--- util.js
+++ util.js
@@ -4,23 +4,21 @@ const token = '1900703486:AAElE2nXiShEtnkkqi-43GJ4fzcPEMw-HS8'
 const bot = new TelegramBot(token, {polling: true})
 bot.setMyCommands([{command: "song", description:"Download a song"}, {command: "search", description:"Search for artist/album/song"}, {command: "moods", description:"Explore music according to mood"}])
 const request = require('request-promise')
+const { Puppeteer } = require('puppeteer/lib/api')
 
 var activesearchrequests = {}
 var activemoodrequests = {}
 
-var browser = ""
+var browser
 async function launchbrowser(){
 //   browser = await puppeteer.launch({headless:true})
-  browser = await puppeteer.connect({
-	  browserWSEndpoint: 'https://browserless-production-72c2.up.railway.app'
+	browser = await puppeteer.connect({
+		browserWSEndpoint: 'https://browserless-production-72c2.up.railway.app'
 	//   browserWSEndpoint: 'wss://chrome.browserless.io?token=a8b718c5-02ab-46c2-bc24-7afa3773659c'
-  });
-  browser.on('disconnected', async ()=>{
-	  browser = await puppeteer.connect({
-		  browserWSEndpoint: 'https://browserless-production-72c2.up.railway.app'
-		//   browserWSEndpoint: 'wss://chrome.browserless.io?token=a8b718c5-02ab-46c2-bc24-7afa3773659c'
-	  });
-  })
+	});
+	browser.on('disconnected', async ()=>{
+		launchbrowser()
+	})
 }
 launchbrowser()
 
@@ -75,39 +73,8 @@ async function getsongs(input, num = 10, iteration = 1){
 async function sendmusicbyname(chatid, name, messageid=undefined) {
   try{
     console.log("Sending music by name - "+name+" to "+chatid)
-    // const page = await browser.newPage()
-    // await page.setRequestInterception(true);
-    // page.on('request', request=>{
-    //   if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
-    //     request.abort();
-    //   } 
-    //   else{
-    //     request.continue();
-    //   }
-    // })
 	const song = await getsong(name)
 	sendmusicbyurl(chatid, song.youtubelink)
-//     bot.sendMessage(chatid, "◂◌◌◌▸")
-//     const [song, response] = await Promise.all([
-//       getsong(name),
-//       page.goto('https://ytmp3.cc/downloader/')
-//     ])
-//     console.log(song)
-//     bot.sendMessage(chatid, "◂●◌◌▸")
-//     await page.type('#input', `https://youtube.com${song.youtubelink}`)
-//     await page.click('#submit')
-//     bot.sendMessage(chatid, "◂●●◌▸")
-//     // await page.waitForTimeout(2000)
-// 	await new Promise(x=>setTimeout(x, 2000))
-//     song.downloadlink = await page.evaluate(function(){
-//       return document.querySelector('#buttons > a').getAttribute('href')
-//     })
-//     await page.close()
-//     console.log(song.downloadlink)
-//     bot.sendMessage(chatid, "◂●●●▸")
-//     bot.sendAudio(chatid, song.downloadlink, {reply_to_message_id: messageid}).then(()=>{
-//       bot.sendMessage(chatid, "Here's "+ song.title)
-//     })
   }
   catch(err){
     console.log("Got an error" + err)
@@ -116,29 +83,8 @@ async function sendmusicbyname(chatid, name, messageid=undefined) {
 }
 
 async function sendmusicbyurl(chatid, youtubelink, isytmusic=false){
-	bot.sendMessage(chatid, 'This may take upto a minute')
-	// youtubedl(youtubelink, {
-	// dumpSingleJson: true,
-	// noCheckCertificates: true,
-	// noWarnings: true,
-	// // listFormats:true
-	// f:'m4a',
-	// o:'audio.m4a'
-	// }).then(output => {
-	// 	// console.log('a',output)
-	// 	// console.log('b',output.requested_downloads)
-	// 	console.log('c',output?.requested_downloads[0].url)
-	// 	// console.log('c',output?.requested_downloads[0]?.requested_formats.find(x=>x.format!=null && x.format.includes('audio')).url)
-	// 	const downloadlink = 'harmonybot-production.up.railway.app/'+createDownloadLink(output?.requested_downloads[0].url)
-		
-	// 	bot.sendAudio(chatid, downloadlink).then(()=>{
-	// 		bot.sendMessage(chatid, "Here's the audio")
-	// 	}).catch((err)=>{
-	// 		console.log("Got an error" + err)
-	// 		bot.sendMessage(chatid, "I seem to have failed at that, please try again")
-	// 	})
-	// })
   try{
+	bot.sendMessage(chatid, 'This may take upto a minute')
     console.log("Sending music by url to "+chatid)
     const page = await browser.newPage()
     await page.setRequestInterception(true);
@@ -197,42 +143,47 @@ async function sendsearchresults(chatid, name, messageid=undefined, iteration=1,
     }else{
       delete activesearchrequests[chatid]
     }
-	console.log(buttons)
     bot.sendMessage(chatid, reply, {reply_to_message_id: messageid, reply_markup: { inline_keyboard: buttons }})
 }
 async function sendmoods(chatid, messageid=undefined){
-  const page = await browser.newPage()
-  activemoodrequests[chatid] = {page:page}
-  page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67')
-  await page.setRequestInterception(true)
-  page.on('request', request=>{
-      if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) != -1) {
-        request.abort();
-      } 
-      else{
-        request.continue();
-      }
-  })
-  await page.goto("https://music.youtube.com/moods_and_genres", )
-  
-  await page.screenshot({path:'./here.png'})
-  const moods = await page.evaluate(function(){
-    const moodbuttons = document.querySelector('#contents #items').querySelectorAll('button')
-    const moods = []
-    for(let i = 0; i<moodbuttons.length; i++){
-        moods.push({name:moodbuttons[i].lastElementChild.innerHTML, buttonindex:i})
-    }
-    console.log(moods)
-    return moods
-  })
-  var buttons = [[]]
-  for(let i =0; i<moods.length; i++){
-    buttons[Math.floor(i/3)].push({text:moods[i].name, callback_data:"mood"+i})
-    if((i+1)%3 == 0){
-      buttons.push([])
-    }
-  }
-  bot.sendMessage(chatid,"Moods:", {reply_markup : {inline_keyboard : buttons}})
+	try{
+		const page = await browser.newPage()
+		activemoodrequests[chatid] = {page:page}
+		page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67')
+		await page.setRequestInterception(true)
+		page.on('request', request=>{
+			if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) != -1) {
+			  request.abort();
+			} 
+			else{
+			  request.continue();
+			}
+		})
+		await page.goto("https://music.youtube.com/moods_and_genres", )
+		
+		await page.screenshot({path:'./here.png'})
+		const moods = await page.evaluate(function(){
+		  const moodbuttons = document.querySelector('#contents #items').querySelectorAll('button')
+		  const moods = []
+		  for(let i = 0; i<moodbuttons.length; i++){
+			  moods.push({name:moodbuttons[i].lastElementChild.innerHTML, buttonindex:i})
+		  }
+		  console.log(moods)
+		  return moods
+		})
+		var buttons = [[]]
+		for(let i =0; i<moods.length; i++){
+		  buttons[Math.floor(i/3)].push({text:moods[i].name, callback_data:"mood"+i})
+		  if((i+1)%3 == 0){
+			buttons.push([])
+		  }
+		}
+		bot.sendMessage(chatid,"Moods:", {reply_markup : {inline_keyboard : buttons}})
+	}
+	catch(err){
+		bot.sendMessage(chatid, 'I seem to have failed, please try again')
+		console.log(err)
+	}
 }
 
 async function sendplaylists(chatid, moodnumber){
